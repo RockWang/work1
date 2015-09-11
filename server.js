1210b66a00303bac0ffe9acd3668512a6a51e263
RockWang <autumn1944@sohu.com>
2015-09-11 22:46:09 +0800

    -'update'

--- server.js
+++ server.js
@@ -1,100 +1,104 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var url = require("url");
-var mime = require('mime');   //文件的类型
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
 
-function removeP(path){    //删除方法
+function deleteP(path){
+    var paths = path.split('/');
+    for(var i =paths.length;i>=1;i--){
+        var loca = paths.slice(0,i).join('/');
+        if(fs.existsSync(path)){
+            if(fs.statSync(loca).isDirectory()){
+                var files = fs.readdirSync(loca);
+                console.log(files)
+                files.forEach(function(files){
+                    var filePath = loca+'/'+files;
+                    console.log(filePath)
+                    if(fs.statSync(filePath).isFile()){ //判断里面的是文件
 
-    var paths=path.split('/');
-    for(i=0; i<paths.length; i++){
-        var p=paths.slice(0,paths.length-i).join('/');
-        var exist=fs.existsSync(p);
-        if(!exist) continue;
-        fs.rmdirSync(p);
-    }
-}
-var server = http.createServer(function (req, res) {
-
-    //获取文件的路径
-    var filePath = path.resolve();
-
-    //icon请求忽略
-    if (req.url == '/favicon.ico') {
-        res.end();
-        return;
-    }
-    //console.log(req.url);
-    if(req.url == '/?'){
-        res.write('已删除！');
-    }
-    /*if(curl.indexOf('?del=')==-1){
-        res.write('ok');
-    }*/
-    //系统份文件分隔符
-    //var sep=path.sep;
-
-    //访问请求的文件地址标准化
-    var reqPath = path.normalize(req.url);
-
-    // var reqPath =req.url;
-
-
-    //获取当文件的路径
-    var pathname = path.join(filePath, reqPath);
-
-    //判断文件是否存在
-    fs.exists(pathname, function (exists) {
-
-        //文件存在
-        if (exists) {
-
-            //判断是否是目录
-            if (fs.statSync(pathname).isDirectory()) {
-                var addStr = '<link rel="stylesheet" href="/public/css/index.css"/>';
-                addStr += '<h1>FileManager system directory</h1>';
-                addStr += '<ul>';
-
-                //遍历文件
-                fs.readdir(pathname, function (err, files) {
-                    res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
-
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        files.forEach(function (file) {
-                            if (pathname != filePath) {
-                                pathname = pathname.replace(filePath, "");
-                            }
-                            filePath1 = path.join(pathname, file).replace(/\\/g, "/");
-
-                            if (path.extname(file)) {
-                                addStr += '<li class="gray"><a href="' + filePath1 + '" style="">' + file + ' </a><a class="del" href="./?' + file + '">删除</a></li>';
-                            } else {
-                                addStr += '<li ><a href="' + filePath1 + '" style="">' + file + '</a><a class="del" onclick="removeP('+ filePath1 +')" href="./?">删除</a></li>';
-                            }
-                        });
-                    }
-                    res.end(addStr + "</ul><p>提示：以上目录列表，蓝色是文件夹，可点击继续进入下一节。</p>");
-                });
-            } else if (fs.statSync(pathname).isFile()) {
-
-                //当访问的是文件时，判断文件类型，并读文件
-                res.writeHead(200, {'Content-Type': mime.lookup(path.basename(pathname)) + ';charset=utf-8'});
-                fs.readFile(pathname, {flag: "r"}, function (err, data) {
-                    if (err) {
-                        res.end(err);
-                    } else {
-                        res.end(data);
+                        fs.unlinkSync(filePath);
+                    }else{
+                        deleteP(filePath)
                     }
                 });
+                fs.rmdirSync(path); //删除空文件夹
+            }else{
+                fs.unlinkSync(loca)
             }
-        } else {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.write('<span style="color:red">"' + pathname + '"</span> was not found on this server.');
-            res.end();
         }
-    });
+    }
 
-});
-server.listen(8080);
+}
+
+http.createServer(function(req,res){
+    var url = req.url;
+    var urls = url.split('?');
+    var pathname = urls[0];
+    var query = urls[1];
+    var queryObj = {};
+    if(query){
+        var field = query.split('&');
+        field.forEach(function(field){
+            var vals = field.split('=');
+            queryObj[vals[0]]=vals[1];
+        })
+    }
+
+    if(pathname=='/favicon.ico'){
+        res.end('');
+    }else if(pathname=='/del'){ //删除事件
+        for(var key in queryObj){
+            deleteP(queryObj[key]);
+        }
+
+    }else{
+        var filename = '.'+pathname;
+        fs.exists(filename,function(exists){ //判断文件是否存在
+            if(exists){ //文件存在
+                var htmlStr = '<link href="/public/css/index.css" rel="stylesheet" />';
+                htmlStr += "<h1>静态目录、文件删除、查看</h1><ul>";
+                if(fs.statSync(filename).isDirectory()){ // 判断是目录
+                    fs.readdir(filename,function(err,files){
+                        if(err){
+                            res.end('error')
+                        }else{
+                            res.writeHeader(200,{'Content-Type':'text/html;charset=utf-8'})
+                            files.forEach(function(file){ //遍历目录下的文件
+                                if(file=='.git'||file=='node_modules'||file=='.idea'){
+                                }else{
+                                    var filePath = filename+'/'+file;
+                                    if(pathname=='/'){//根目录
+                                        if(fs.statSync(filePath).isDirectory()){
+                                            htmlStr+='<li class="folder"><a href="'+file+'">'+file+'</a><a href=/del?path='+file+' class="del">删除</a> </li>';
+                                        }else{
+                                            htmlStr+='<li><a href="'+file+'">'+file+'</a><a href=/del?path='+file+' class="del">删除</a> </li>';
+                                        }
+                                    }else{ //根目录下一级目录
+                                        if(fs.statSync(filePath).isDirectory()){
+                                            htmlStr+='<li class="folder"><a href="'+pathname+'/'+file+'">'+file+'</a><a href=/del?path='+pathname+'/'+file+' class="del">删除</a> </li>';
+                                        }else{
+                                            htmlStr+='<li><a href="'+pathname+'/'+file+'">'+file+'</a><a href=/del?path='+pathname+'/'+file+' class="del">删除</a> </li>';
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        htmlStr+='</ul>'
+                        res.end(htmlStr)
+                    })
+                }else if(fs.statSync(filename).isFile()){ //如果是文件
+                    res.writeHeader(200,{'Content-Type':mime.lookup(filename)+';charset=utf-8'});
+                    fs.readFile(filename,function(err,data){
+                        res.end(data);
+                    })
+                }else{
+                    res.writeHeader(400,{'Content-Type':'text/html;charset=utf-8'});
+                    res.end('404 not found')
+                }
+            }
+        })
+
+    }
+
+}).listen(8080)
